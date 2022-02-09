c53eb1dc32599fdda83efa0d6b15a29d24ec7cf7
avvasil <98763717+avvasil@users.noreply.github.com>
2022-02-09 08:20:31 +0300

    Update src/pro/sky/Main.java
    better to reduce
    
    for (int i = 0; i <= arr.length - 1; i++) {
    
    for (int i = 0; i < arr.length; i++) {
    
    Co-authored-by: OKrylov <krylov92oa@gmail.com>

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int minSpendingPerDay = 200001;
         int maxSpendingPerDay = 99999;
 
-        for (int i = 0; i <= arr.length - 1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] < minSpendingPerDay)
                 minSpendingPerDay = arr[i];
         }
